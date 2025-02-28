6ade46adf3c726435c58ed7dd01c44757318eba2
tabriz.gulmammadov <tabriz.gulmammadov@azai.tech>
2025-02-28 16:50:54 +0400

    feat: Init project

---
+++ examples/visits/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const redis = require("redis");
+
+const app = express();
+const redisClient = redis.createClient({
+    host: "redis-server",
+    port: 6379
+});
+redisClient.set("visits", 0);
+
+app.get("/", (req, res) => {
+  redisClient.get("visits", (err, visits) => {
+    res.send("Number of visits is " + visits);
+    redisClient.set("visits", parseInt(visits) + 1);
+  });
+});
+
+app.listen(8081, () => {
+  console.log("Server is running on port 8081");
+});
